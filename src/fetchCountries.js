e1d7be8e4c94852ba887fc04f3c3b07a70e9b15e
Dmytro Petyshyn <dev6012@gmail.com>
2023-05-18 11:23:52 +0300

    async func added

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,13 +1,12 @@
 const API = 'https://restcountries.com/v3.1/name/';
 
-function fetchCountryByName(countryName) {
+async function fetchCountryByName(countryName) {
   const url = `${API}${countryName}?fields=name,capital,population,flags,languages`;
-  return fetch(url).then(response => {
-    if (!response.ok) {
-      throw new Error(response.status);
-    }
-    return response.json();
-  });
+  const response = await fetch(url);
+  if (!response.ok) {
+    throw new Error(response.status);
+  }
+  return await response.json();
 }
 
 export { fetchCountryByName };
